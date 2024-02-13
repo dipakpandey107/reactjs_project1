54fabaf21ae9ac0e5acdfbc1b87b048d0b01e408
Dipak Pandey <133677469+dipakpandey107@users.noreply.github.com>
2024-02-13 23:21:11 +0530

    Continue Header Part

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+   <nav>
+    <a href='/'>Home</a>
+    <a href='/about'>About</a>
+    <a href='/contact'>Contact</a>
+    
+   </nav>
+  )
+}
+
+export default Header;
