b313267e7abc5a9f7ee1cecc715939e97b0bb630
Dipak Pandey <133677469+dipakpandey107@users.noreply.github.com>
2024-02-15 23:57:34 +0530

    header and home page working perfectly

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
 
 const Header = () => {
   return (
-   <nav>
-    <a href='/'>Home</a>
-    <a href='/about'>About</a>
-    <a href='/contact'>Contact</a>
-    
-   </nav>
-  )
-}
+    <nav>
+      <h1>Glacier.</h1>
+      <main>
+        <HashLink to={"/#home"}>Home</HashLink>
+        <Link to={"/contact"}>Contact</Link>
+        <HashLink to={"/#about"}>About</HashLink>
+        <HashLink to={"/#brands"}>Brands</HashLink>
+        <Link to={"/services"}>Services</Link>
+      </main>
+    </nav>
+  );
+};
 
 export default Header;
